e9c7911d0033eb48d8b8f3abe41c3e2fe72198fa
DanLe <danle.sdev@gmail.com>
2020-05-27 12:05:51 +0700

    Add form animations

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:onboardingconcept/constants.dart';
 import 'package:onboardingconcept/onboarding_page.dart';
 
@@ -27,7 +30,8 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Stack(
-        children: [
+        children: <Widget>[
+          buildForms(context),
           buildSecondBackgroundCurve(context),
           buildFirstBackgroundCurve(context),
           buildHeader(context),
@@ -41,8 +45,8 @@ class _LoginPageState extends State<LoginPage> {
     return ClipPath(
       clipper: CurveClipper(
         lowerHeightRatio: 1.0,
-        upperHeightRatio: 0.38,
-        rightOffset: 50,
+        upperHeightRatio: 0.3,
+        rightOffset: 35,
         animation: CurvedAnimation(
             parent: animation, curve: Interval(0.3, 0.8, curve: Curves.easeIn)),
       ),
@@ -57,46 +61,36 @@ class _LoginPageState extends State<LoginPage> {
           children: [
             FadeTransition(
                 opacity:
-                    CurvedAnimation(parent: animation, curve: Interval(0, 0.2)),
+                    CurvedAnimation(parent: animation, curve: Interval(0, 0.3)),
                 child: Logo(
-                  size: 62,
+                  size: 56,
                 )),
             Container(
               height: 28,
             ),
-            FadeTransition(
-              opacity: Tween<double>(begin: 0.0, end: 1.0).animate(
-                  CurvedAnimation(
-                      parent: animation, curve: Interval(0.1, 0.4))),
-              child: SlideTransition(
-                position: Tween<Offset>(begin: Offset(0, 1), end: Offset.zero)
-                    .animate(CurvedAnimation(
-                        parent: animation, curve: Interval(0.1, 0.4))),
-                child: Text(
-                  "Welcome to Busble",
-                  style: TextStyle(
-                      fontSize: 30,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold),
-                ),
+            FadeAndSlideInWidget(
+              animation:
+                  CurvedAnimation(parent: animation, curve: Interval(0.1, 0.4)),
+              slideOffset: Offset(0, 1),
+              child: Text(
+                "Welcome to Facebook",
+                style: TextStyle(
+                    fontSize: 26,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
               ),
             ),
             Container(
-              height: 10,
+              height: 8,
             ),
-            FadeTransition(
-              opacity: Tween<double>(begin: 0.0, end: 1.0).animate(
-                  CurvedAnimation(
-                      parent: animation, curve: Interval(0.1, 0.4))),
-              child: SlideTransition(
-                position: Tween<Offset>(begin: Offset(0, 1.5), end: Offset.zero)
-                    .animate(CurvedAnimation(
-                        parent: animation, curve: Interval(0.1, 0.4))),
-                child: Text(
-                  "Lorem ipsum dolor sit conseteur\nsadipscing elitr, sed diam",
-                  style: TextStyle(
-                      fontSize: 18, color: Colors.grey[600], height: 1.5),
-                ),
+            FadeAndSlideInWidget(
+              animation:
+                  CurvedAnimation(parent: animation, curve: Interval(0.1, 0.4)),
+              slideOffset: Offset(0, 1.5),
+              child: Text(
+                "Lorem ipsum dolor sit conseteur\nsadipscing elitr, sed diam",
+                style: TextStyle(
+                    fontSize: 16, color: Colors.grey[600], height: 1.5),
               ),
             ),
           ],
@@ -110,11 +104,15 @@ class _LoginPageState extends State<LoginPage> {
     return ClipPath(
       clipper: CurveClipper(
           lowerHeightRatio: 1.0,
-          upperHeightRatio: 0.44,
-          rightOffset: 80,
+          upperHeightRatio: 0.355,
+          rightOffset: 66,
           animation: CurvedAnimation(
               parent: animation,
-              curve: Interval(0.5, 0.85, curve: Curves.easeIn))),
+              curve: Interval(
+                0.5,
+                0.8,
+                curve: Curves.easeIn,
+              ))),
       child: Container(
         color: kBlue,
       ),
@@ -126,13 +124,225 @@ class _LoginPageState extends State<LoginPage> {
     return ClipPath(
       clipper: CurveClipper(
           lowerHeightRatio: 2.0,
-          upperHeightRatio: 0.51,
+          upperHeightRatio: 0.41,
           rightOffset: 100,
           animation: CurvedAnimation(
               parent: animation,
-              curve: Interval(0.6, 0.9, curve: Curves.easeIn))),
+              curve: Interval(0.6, 0.8, curve: Curves.easeIn))),
       child: Container(
         color: kGrey,
+      ),
+    );
+  }
+
+  Widget buildForms(BuildContext context) {
+    final animation = ModalRoute.of(context).animation;
+    final curvedAnimation = CurvedAnimation(
+        parent: animation, curve: Interval(0.82, 1.0, curve: Curves.easeInOut));
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: kPaddingL),
+      child: Column(
+        children: [
+          Spacer(
+            flex: 3,
+          ),
+          Expanded(
+            flex: 4,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                FadeAndSlideInWidget(
+                  slideOffset: Offset(0, 0.5),
+                  animation: curvedAnimation,
+                  child: InputTextField(
+                    icon: Icons.person,
+                    text: "Username or Email",
+                  ),
+                ),
+                Container(
+                  height: 16,
+                ),
+                FadeAndSlideInWidget(
+                  slideOffset: Offset(0, 0.75),
+                  animation: curvedAnimation,
+                  child: InputTextField(
+                    icon: Icons.lock,
+                    text: "Password",
+                  ),
+                ),
+                Container(
+                  height: 16,
+                ),
+                FadeAndSlideInWidget(
+                    slideOffset: Offset(0, 1),
+                    animation: curvedAnimation,
+                    child: LoginButton()),
+                Container(
+                  height: 48,
+                ),
+                FadeAndSlideInWidget(
+                    slideOffset: Offset(0, 1.25),
+                    animation: curvedAnimation,
+                    child: GoogleButton()),
+                Container(
+                  height: 24,
+                ),
+                FadeAndSlideInWidget(
+                    slideOffset: Offset(0, 1.5),
+                    animation: curvedAnimation,
+                    child: CreateAccountButton()),
+              ],
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).padding.bottom,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FadeAndSlideInWidget extends StatelessWidget {
+  final Animation<double> animation;
+  final Widget child;
+  final Offset slideOffset;
+
+  const FadeAndSlideInWidget(
+      {Key key, this.animation, this.child, this.slideOffset})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: Tween<double>(begin: 0.0, end: 1.0).animate(animation),
+      child: SlideTransition(
+        position: Tween<Offset>(begin: slideOffset, end: Offset.zero)
+            .animate(animation),
+        child: child,
+      ),
+    );
+  }
+}
+
+class GoogleButton extends StatelessWidget {
+  const GoogleButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Positioned(
+            left: kPaddingM,
+            child: SvgPicture.network(
+              "https://image.flaticon.com/icons/svg/2702/2702602.svg",
+              height: 26,
+            )),
+        Container(
+          height: 60,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4),
+            border: Border.all(
+                color: Colors.grey[300].withOpacity(0.8), width: 1.5),
+          ),
+          alignment: Alignment.center,
+          child: Text(
+            "Continue with Google",
+            style: TextStyle(
+                color: Colors.grey.shade500,
+                fontSize: 16,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CreateAccountButton extends StatelessWidget {
+  const CreateAccountButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.black87,
+        borderRadius: BorderRadius.circular(4),
+      ),
+      alignment: Alignment.center,
+      child: Text(
+        "Create an account",
+        style:
+            TextStyle(color: kWhite, fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: kBlue,
+        borderRadius: BorderRadius.circular(4),
+      ),
+      alignment: Alignment.center,
+      child: Text(
+        "Login to continue",
+        style:
+            TextStyle(color: kWhite, fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
+
+class InputTextField extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const InputTextField({
+    Key key,
+    this.icon,
+    this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final border = OutlineInputBorder(
+      borderSide:
+          BorderSide(width: 1.5, color: Colors.grey[300].withOpacity(0.8)),
+      borderRadius: BorderRadius.circular(4),
+    );
+    return Theme(
+      data: Theme.of(context).copyWith(
+          primaryColor: kLightBlue, textSelectionColor: Colors.grey[300]),
+      child: TextField(
+        cursorColor: kLightBlue,
+        decoration: InputDecoration(
+          hintText: text,
+          hintStyle: TextStyle(color: Colors.grey.shade500),
+          prefixIcon: Icon(
+            icon,
+            size: 22,
+          ),
+          border: border,
+          enabledBorder: border,
+        ),
       ),
     );
   }
@@ -160,7 +370,7 @@ class CurveClipper extends CustomClipper<Path> {
             .evaluate(animation);
     path.lineTo(0, height);
     path.quadraticBezierTo(
-        size.width * 1 / 3, height + 50, size.width, height - rightOffset);
+        size.width * 0.4, height + 40, size.width, height - rightOffset);
     path.lineTo(size.width, 0);
     path.close();
     return path;
